b83ab7af609ae743ba451b03347fa8588bab497a
prism <2839296425@qq.com>
2018-09-29 16:46:07 +0800

    给“硬件”链接加上了html页面 附上了一个问题

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     # http://localhost:8000/blog/
     path('', views.blog_list, name='blog_list'),
+    path('hw',views.hardware, name='hardware'),
     path('<int:blog_pk>', views.blog_detail, name="blog_detail"),
     path('type/<int:blog_type_pk>', views.blogs_with_type, name="blogs_with_type"),
     path('date/<int:year>/<int:month>', views.blogs_with_date, name="blogs_with_date"),
